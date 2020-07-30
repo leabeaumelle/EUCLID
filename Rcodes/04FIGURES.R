487961fcface8531e805557d5b90d372172bab9a
LeaBeaumelle <lea.beaumelle@inrae.fr>
2020-07-30 10:40:39 +0200

    Make figure 1, adding panel for predation rates

--- Rcodes/04FIGURES.R
+++ Rcodes/04FIGURES.R
@@ -11,11 +11,11 @@ library(viridis)
 ## Data----------------------------------------------------------------------------
 Abundance <- read.csv("Output/AbundanceClean.csv")
 Diversity <- read.csv("Output/DiversityClean.csv")
-# Preddata <- read.csv("Output/PredationPest_clean.csv")
+Pred <- read.csv("Output/PredationPest_clean.csv")
 
 
 
-## Figure 1 - overall effect of flower strips on natural enemies and predation
+## Figure 1 - overall effect of flower strips on natural enemies and predation------------
 # three panels with overall flower strip effect across landscapes and communitiy types
 
 # Panel A - Flower strips increase the abundance of natural enemies
@@ -41,7 +41,7 @@ F1A <- ggplot(data = Abundance, aes(y=Total, x=Treatment, fill = Treatment))+
         axis.title.x = element_blank())
 F1A
 
-# Panel A - Flower strips increase the abundance of natural enemies
+# Panel B - Flower strips don't change the diversity of natural enemies (genus richness)
 
 # reorder levels of treatment
 Diversity$Treatment <- relevel(Diversity$Treatment, ref = "Low Div")
@@ -65,17 +65,41 @@ F2A <- ggplot(data = Diversity[!is.na(Diversity$GenusR),], aes(y=GenusR, x=Treat
 F2A
 
 
+# Panel C - Flower strips effect on predation depends on the landscape
+
+# reorder levels of treatment
+Pred$Treatment <- relevel(Pred$Treatment, ref = "Low Div")
+
+# set sizes of text in plots
+sizetext <- 10
+sizelegend <- 8
+
+F2C <- ggplot(data = Pred[!is.na(Pred$PredRate),], aes(y=PredRate, x=Treatment, fill = Treatment))+
+  geom_boxplot()+
+  geom_jitter(width = .25, alpha = .4, size = 0.4)+
+  scale_fill_viridis_d(begin = 0.5)+
+  # scale_fill_manual(values = c(pal[2], pal[3]))+
+  ylab("Predation Rates")+
+  theme_bw()+
+  theme(legend.position = "none", 
+        axis.text.y=element_text(face = "bold", size = sizelegend),
+        axis.text.x=element_text(size = sizetext),
+        axis.title.y = element_text(size=sizetext, face = "bold"),
+        axis.title.x = element_blank())
+F2C
+
+
 # save a png with high res
 ppi <- 300# final: 600 # resolution
 w <- 20 # width in cm
 
 png("Figures/Fig1.png",
-    width=2*(w/3),
+    width=w,
     height=w/3,
     units = "cm",
     res=ppi)
 
-F1A+F2A
+F1A+F2A+F2C
 
 dev.off()
 
